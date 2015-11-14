db1684c73e24757fc720f343a9c309193df2c219
Nicolas Helgeson <afkupuz72@yahoo.com>
2015-11-14 10:42:00 -0800

    Create media.py

---
+++ media.py
@@ -0,0 +1,15 @@
+#import for class function
+import webbrowser
+
+#class with initializer
+class Movie:
+    def __init__(self, title, description, poster_image_url, trailer_youtube_url):
+        self.title = title
+        self.description = description
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
+
+    def open_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
+
+
